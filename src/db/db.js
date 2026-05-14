80c39132d9f5684ed0df22beb04cb25ca22149ab
Ghanshyam <gc.cse28@gmail.com>
2026-05-14 10:38:35 +0530

    Update MongoDB connection to use environment variable

--- src/db/db.js
+++ src/db/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose")
 
 
 async function connectDB() {
-    await mongoose.connect("mongodb+srv://database-vala-part:dd8mea7Ah6RhBDAP@sou17.gepti3u.mongodb.net/halley")
+    await mongoose.connect(process.env.MONGODB_URI)
 
     console.log(" connected to DB ")
 }
